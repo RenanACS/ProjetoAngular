8f1f1977ef8f7b24f89890eedc8d20f095a32e50
João Borges <jpborgesn@gmail.com>
2026-03-20 09:43:05 -0300

    acesso-portal

---
+++ src/app/acesso-portal/acesso-portal.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-acesso-portal',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './acesso-portal.html',
+  styleUrl: './acesso-portal.css'
+})
+export class AcessoPortal {
+
+  modo: 'login' | 'cadastro' = 'login';
+
+  email: string = '';
+  senha: string = '';
+  nome: string = '';
+
+  trocarModo(novoModo: 'login' | 'cadastro') {
+    this.modo = novoModo;
+  }
+
+  login() {
+    if (this.email && this.senha) {
+      alert('Login realizado com sucesso!');
+    } else {
+      alert('Preencha todos os campos!');
+    }
+  }
+
+  cadastrar() {
+    if (this.nome && this.email && this.senha) {
+      alert('Conta criada com sucesso!');
+      this.modo = 'login';
+    } else {
+      alert('Preencha todos os campos!');
+    }
+  }
+
+}
